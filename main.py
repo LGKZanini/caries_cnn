a8b0f4759135a58c1c1c462aafbea9d1fba7d0a7
Luiz Zanini <luiz.kasputis@hotmail.com>
2023-05-16 09:54:25 -0300

    update memory and change device

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == '__main__' :
     
     batch_size = int(sys.argv[1])
     
-    os.environ['gpu'] = '0'
+    os.environ['gpu'] = '1'
     
     train_simple(epochs=50, batch_size=batch_size, folds=5)
     
